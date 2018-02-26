5a6970ff72b433072e06e27dd28804e44413e7a7
Administrator <jiaojinda@163.COM>
2018-02-26 11:23:15 +0800

    SpringBootThymeleafJPA upload

---
+++ src/main/java/com/jiao/SpringBootThymeleafJpaApplication.java
@@ -0,0 +1,12 @@
+package com.jiao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootThymeleafJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootThymeleafJpaApplication.class, args);
+	}
+}
